95d947f31294587ddc05075baaaa4562407559c7
Timothy Kamau <teamorekamau97@gmail.com>
2021-02-25 15:08:48 +0000

    Implement queue with ll

---
+++ queue_ll.py
@@ -0,0 +1,83 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def is_empty(self):
+        if not self.head:
+            return True
+        else:
+            False
+
+    def create(self, value):
+        node = Node(value)
+        self.head = node
+        self.tail = node
+        return value
+
+    def enque(self, value):
+        if self.is_empty():
+            return self.create(value)
+        else:
+            new_node = Node(value)
+            self.tail.next = new_node
+            self.tail = new_node
+            return value
+
+    def deque(self):
+        if self.is_empty():
+            return "The queue is empty"
+        else:
+            head = self.head
+            self.head = head.next
+            return head.value
+
+    def peek(self):
+        if self.is_empty():
+            return "The queue is empty"
+        else:
+            return self.head.value
+
+    def delete(self):
+        self.head = None
+        self.tail = None
+        return "Queue deleted successfully"
+
+    def __str__(self):
+        if self.is_empty():
+            return "The queue is empty"
+        else:
+            node = self.head
+            res = []
+
+            while node:
+                res.append(str(node.value))
+                node = node.next
+
+            return '\n'.join(res)
+        
+
+q = Queue()
+print("===enque===")
+print(q.enque(1))
+print("===enque===")
+print(q.enque(2))
+print("===enque===")
+print(q.enque(3))
+print("===enque===")
+print(q.enque(4))
+print("===deque===")
+print(q.deque())
+print("===peek===")
+print(q.peek())
+print("===print===")
+print(q)
+print("===delete===")
+print(q.delete())
+print("===print===")
+print(q)
